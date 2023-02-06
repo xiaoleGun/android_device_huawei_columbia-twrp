fdd21f38e23da06811d2bb2b3699cffd4245c3aa
xiaoleGun <1592501605@qq.com>
2023-02-06 18:41:45 +0800

    columbia: Set ALLOW_MISSING_DEPENDENCIES to true
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Build rules
+ALLOW_MISSING_DEPENDENCIES := true
+
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
